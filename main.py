ca4d3fe678753538305c5b8e72ff3c4f8b7e2f23
Vittorja <vittorija13@gmail.com>
2023-11-20 23:29:52 +0100

    first enit

---
+++ main.py
@@ -0,0 +1,25 @@
+list_1 = [1, 2, 3, 4, 5, 6]
+list_2 = [1, 2, 3]
+list_3 = [1, 2, 3, 4, 5]
+list_4 = [1]
+list_5 = []
+mid_1 = len(list_1) // 2
+resul_1 = list_1[:mid_1 + len(list_1) % 2]
+resul_2 = list_1[mid_1 + len(list_1) % 2:]
+print(resul_1, resul_2)
+mid_2 = len(list_2) // 2
+resul_3 = list_2[:mid_2 + len(list_2) % 2]
+resul_4 = list_2[mid_2 + len(list_2) % 2:]
+print(resul_3, resul_4)
+mid_3 = len(list_3) // 2
+resul_5 = list_3[:mid_3 + len(list_3) % 2]
+resul_6 = list_3[mid_3 + len(list_3) % 2:]
+print(resul_5, resul_6)
+mid_4 = len(list_4) // 2
+resul_7 = list_4[:mid_4 + len(list_4) % 2]
+resul_8 = list_4[mid_4 + len(list_4) % 2:]
+print(resul_7, resul_8)
+mid_5 = len(list_5) // 2
+resul_9 = list_5[:mid_5 + len(list_5) % 2]
+resul_10 = list_5[mid_5 + len(list_5) % 2:]
+print(resul_9, resul_10)
